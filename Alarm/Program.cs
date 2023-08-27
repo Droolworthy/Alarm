6e08424140e2b910a16a616d22dca6026b54a6e2
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2023-08-28 00:22:04 +0300

    Добавьте файлы проекта.

---
+++ Alarm/Program.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SoundControl))]
+public class Alarm : MonoBehaviour
+{
+    private SoundControl _soundControl;
+
+    private void Start()
+    {
+        _soundControl = GetComponent<SoundControl>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        int soundLevel = 1;
+
+        if (collision.TryGetComponent<Player>(out Player player))
+        {
+            StartCoroutine(_soundControl.ChangeSoundLevel(soundLevel));
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        int soundLevel = 0;
+
+        if (collision.TryGetComponent<Player>(out Player player))
+        {
+            StartCoroutine(_soundControl.ChangeSoundLevel(soundLevel));
+        }
+    }
+}
