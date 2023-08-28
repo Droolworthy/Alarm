4f96f555fb096ba0df268fefb446bd4804c0da1a
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2023-08-28 08:38:01 +0300

    Add files via upload

---
+++ Alarm/Program1.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundControl : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+
+    private readonly float _volume = 0.1f;
+    private readonly float _startValue = 0.001f;
+    private readonly Coroutine _coroutine;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.volume = _startValue;
+    }
+
+    public IEnumerator ChangeSoundLevel(int soundLevel)
+    {
+        EnableAudio();
+
+        yield return TransformSoundLevel(soundLevel);
+
+        DisableAudio();
+
+        StopCoroutine();
+    }
+
+    private void EnableAudio()
+    {
+        if (_audioSource.volume == _startValue)
+        {
+            _audioSource.Play();
+        }
+    }
+
+    private IEnumerator TransformSoundLevel(int soundLevel)
+    {
+        bool isWork = true;
+
+        while (isWork)
+        {
+            if (_audioSource.volume == soundLevel)
+            {
+                isWork = false;
+            }
+            else
+            {
+                _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, soundLevel, Time.deltaTime * _volume);
+            }
+
+            yield return null;
+        }
+    }
+
+    private void DisableAudio()
+    {
+        int minSoundLevel = 0;
+
+        if (_audioSource.volume == minSoundLevel)
+        {
+            _audioSource.Stop();
+        }
+    }
+
+    private void StopCoroutine()
+    {
+        if (_coroutine != null)
+        {
+            StopCoroutine(_coroutine);
+        }
+    }
+}
